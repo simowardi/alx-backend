47a5996d0630c0aad22c2c75fe2e2e1623741b17
simowardi <daoist32@gmail.com>
2024-08-19 12:21:39 +0000

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -6,14 +6,6 @@ from typing import Dict, List, Tuple
 index_range = __import__('0-simple_helper_function').index_range
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
-    """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_inde)
-
-
 class Server:
     """Server class to paginate a database of popular baby names.
     """
